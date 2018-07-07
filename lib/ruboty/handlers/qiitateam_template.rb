a847ba501c8f42c229666fb97ddc517c11518044
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-07-07 22:23:52 +0900

    [braking change] Remember Qiita access token of each user
    Stop using `$QIITA_ACCESS_TOKEN`

--- lib/ruboty/handlers/qiitateam_template.rb
+++ lib/ruboty/handlers/qiitateam_template.rb
@@ -6,13 +6,21 @@ module Ruboty
   module Handlers
     class QiitateamTemplate < Base
       on %r(create template (?<id>\d+) *(?<coediting>coediting)?), name: :create, description: "generate template on #{ENV['ORGANIZATION_NAME']}.qiita.com"
+      on %r(remember my qiita token (?<token>.+)\z), name: "remember", description: "Remember sender's Qiita access token"
 
       def create(message)
+        access_tokens = message.robot.brain.data['qiitateam_template'] || {}
+        access_token = access_tokens[message.from_name]
+        if access_token.nil?
+          message.reply("I don't know your Qiita access token")
+          return
+        end
+
         url = URI.parse("https://#{ENV['ORGANIZATION_NAME']}.qiita.com/api/v2/items")
         req = Net::HTTP::Post.new(url.path)
-        req = set_headers(req)
+        req = set_headers(req, access_token)
 
-        template = template(message[:id])
+        template = template(message[:id], access_token)
         coediting = message[:coediting] ? true : false
         data = {
           coediting: coediting,
@@ -36,11 +44,18 @@ module Ruboty
         message.reply("created #{created_url}")
       end
 
+      def remember(message)
+        access_tokens = message.robot.brain.data['qiitateam_template'] || {}
+        access_tokens[message.from_name] = message[:token]
+        message.robot.brain.data['qiitateam_template'] = access_tokens
+        message.reply("Remembered #{message.from_name}'s Qiita access token")
+      end
+
       private
-      def template(id)
+      def template(id, access_token)
         url = URI.parse("https://#{ENV['ORGANIZATION_NAME']}.qiita.com/api/v2/templates/#{id}")
         req = Net::HTTP::Get.new(url.request_uri)
-        req = set_headers(req)
+        req = set_headers(req, access_token)
 
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
@@ -55,8 +70,7 @@ module Ruboty
         {title: json["expanded_title"], body: json["expanded_body"], tags: json["tags"]}
       end
 
-      def set_headers(req)
-        access_token = ENV['QIITA_ACCESS_TOKEN']
+      def set_headers(req, access_token)
         req['Accept'] = 'application/json'
         req['Content-Type'] = 'application/json; charset=utf-8'
         req['Authorization'] = "Bearer #{access_token}"
